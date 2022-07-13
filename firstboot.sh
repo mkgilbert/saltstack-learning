ccf03c1b572d7e15a2d10eb1aa06223fdccc06dc
Michael Gilbert <mike@nau.edu>
2022-07-13 23:26:11 +0000

    initialization scripts

---
+++ firstboot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#!/bin/bash
+# Download key
+sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
+# Create apt sources list file
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo apt-get update
+sudo apt-get install salt-minion
+
+systemctl disable firstboot.service
+rm -f /etc/systemd/system/firstboot.service
+rm -f /firstboot.sh
